34cddc8335e0f8bfcc421912e7d53031b81a7096
Carlos Eduardo <cb473355@gmail.com>
2025-10-16 19:31:11 -0300

    commit

--- JavaScript/else-Switch-For/for.js
+++ JavaScript/else-Switch-For/for.js
@@ -1,23 +1,29 @@
-const numeros = [1, 2, 3, 4, 5];
+// const numeros = [1, 2, 3, 4, 5];
 
-for(let i = 0; i < numeros.length; i++) {
-  console.log(numeros[i]);
-}
+// for(let i = 0; i < numeros.length; i++) {
+//   console.log(numeros[i]);
+// }
 
-let jogadas = 0;
-let cara = false;
+// for (let i = 1; i <= 10; i++) {
+//   console.log("*".repeat(i))
+// }
 
-while(!cara) {
-  jogadas++;
-  cara = Math.random() < 0.5;
-}
+// let jogadas = 0;
+// let cara = false;
 
-console.log(`A moeda caiu cara após ${jogadas} jogadas!`);
+// while(!cara) {
+//   jogadas++;
+//   cara = Math.random() < 0.5;
+// }
 
-let numero;
+// console.log(`A moeda caiu cara após ${jogadas} jogadas!`);
 
-do {
-  numero = prompt('Digite um número entre 1 e 10:');
-} while(numero < 1 || numero > 10);
+// let numero;
 
-console.log(`Você digitou o número ${numero}.`);
+// do {
+//   numero = prompt('Digite um número entre 1 e 10:');
+// } while(numero < 1 || numero > 10);
+
+// console.log(`Você digitou o número ${numero}.`);
+
+
